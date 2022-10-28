3a3066d92396aea9435b2d6ee4e4ef2897daa526
HTrieuhehe <trieu.huynh@reso.vn>
2022-10-28 09:27:01 +0700

    update: commit all file to github

---
+++ DynamicCasualGameWebsite/WebAPI/LambdaFunction.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>() 
+                .UseLambdaServer();
+        }
+    }
+}
